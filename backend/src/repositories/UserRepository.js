5f73efe44e786d698707e32da37a7d4f6cc696c9
Ronaldo Gomes <ronaldo.gomes@summit-bra.com>
2020-05-07 02:19:04 -0300

    Criado o repositório do Usuário

---
+++ backend/src/repositories/UserRepository.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const User = require('../models/User');
+
+module.exports = {
+    async get (){
+        const users = await User.find('name email');
+        return users;
+    },
+
+    async create (data){
+        await User.create(data);
+    }
+}
