4823584b52f96168ea249d9233475412fc378e4d
Ronaldo Gomes <ronaldo.gomes@summit-bra.com>
2020-05-07 01:45:33 -0300

    Model vehicle criada

---
+++ backend/src/models/Vehicle.js
@@ -0,0 +1,36 @@
+'use strict';
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const VehicleSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        index: true,
+        unique: true
+    },
+    vehicle: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    description: String,
+    created: {
+        type: Date,
+        default: Date.now()
+    },
+    updated: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+VehicleSchema.plugin(mongoosePaginate);
+module.exports = mongoose.model('Vehicle', VehicleSchema);
