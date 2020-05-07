dc0770c63b6c8b42276584943f7f9778c5bb3ef9
Ronaldo Gomes <ronaldo.gomes@summit-bra.com>
2020-05-07 01:48:03 -0300

    Criado repositório do veículo

---
+++ backend/src/repositories/VehicleRepository.js
@@ -0,0 +1,35 @@
+'use strict';
+const Vehicle = require('../models/Vehicle');
+
+module.exports = {
+    async get () {
+        const vehicles = await Vehicle.paginate({}, {page: 1, limit: 10});
+        return vehicles;
+    },
+
+    async getByBrand (brand) {
+        const vehicles = await Vehicle.find({brand: brand });
+        return vehicles;
+    },
+
+    async getById (id){
+        const vehicle = await Vehicle.findOne({id: id});
+        return vehicle;
+    },
+
+    async create (data) {
+        await Vehicle.create(data);
+    },
+
+    async update (id, data) {
+        await Vehicle.findOneAndUpdate(id, data);
+    },
+
+    async updateBySomeFields (id, data){
+        await Vehicle.findOneAndUpdate(id, data);
+    },
+
+    async delete (id) {
+        await Vehicle.findOneAndRemove(id);
+    }
+}
